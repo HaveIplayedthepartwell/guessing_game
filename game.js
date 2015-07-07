5624621ceb8d8b83d72a0a8e65570392ac917983
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-07 14:37:52 -0700

    Added no image picture and text, added the user and computer guesses to the html

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var possibleWeather =
   ["snowy","http://www.clipartpal.com/_thumbs/pd/weather/snow_from_glossy_cloud.png"],
   ["hail","http://image.shutterstock.com/display_pic_with_logo/94868/94868,1185947564,4/stock-photo-hail-cloud-4270675.jpg"],
   ["cloudy","http://cdn.1001freedownloads.com/vector/thumb/122296/weather-overcast.png"],
-  ["foggy","http://cliparts.co/cliparts/dT4/oAo/dT4oAod7c.jpg"]
+  ["foggy","http://cliparts.co/cliparts/dT4/oAo/dT4oAod7c.jpg"],
 ];
 
 //Randomly select a weather condition from the array.
@@ -24,7 +24,7 @@ switch(guess) {
     guess = "rainy";
     break;
   case "sun":
-    guess = "sunny"
+    guess = "sunny";
     break;
   case "snow":
     guess = "snowy";
@@ -48,6 +48,13 @@ var userImageURL;
 for(i = 0; i < possibleWeather.length; i++) {
   if(possibleWeather[i][0] === guess) {
     userImageURL = possibleWeather[i][1];
+    var guess_text = document.getElementById("user_guess_header");
+    guess_text.textContent = "You guessed " + guess + ".";
+    break;
+  } else {
+    var wrong_guess = document.getElementById("user_guess_header");
+    wrong_guess.textContent = "You guessed " + guess + " but I don't have a picture of that.";
+    userImageURL = "http://media.catmoji.com/post/v3af/awww-3-sad-face.jpg";
   }
 }
 userImage.setAttribute("src",userImageURL);
@@ -55,6 +62,8 @@ document.getElementById("user_guess").appendChild(userImage);
 
 //Create the node for the image of the computer's guess
 var answerImage = document.createElement("IMG");
+var computer_guess = document.getElementById("computer_guess");
+    computer_guess.textContent = "The computer guessed " + answer + ".";
 var answerURL = possibleWeather[weatherIndex][1];
 answerImage.setAttribute("src",answerURL);
 document.getElementById("answer").appendChild(answerImage);
